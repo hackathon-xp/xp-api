b58b2152de45adadf3817890b4e4fdbaaf16cee9
Erick Wendel <erickwendel@Ericks-MacBook-Pro.local>
2017-10-22 08:09:47 -0200

    added wallet

--- src/routes/Investment.ts
+++ src/routes/Investment.ts
@@ -44,20 +44,6 @@ export class InvestmentRoute {
    * @returns {Promise<Hapi.ReplyValue>} 
    * @memberof InvestmentRoute
    */
-  public async create(
-    request: Hapi.Request,
-    reply: Hapi.ReplyNoContinue,
-  ): Promise<Hapi.ReplyValue> {
-    try {
-      const investment = await this.investmentModel.create({
-        refId: request.payload.refId,
-      });
-      return reply(investment);
-    } catch (e) {
-      console.log(`ERROR: `, e);
-      return reply(e);
-    }
-  }
 
   public async recommendations(
     request: Hapi.Request,
@@ -86,31 +72,6 @@ export class InvestmentRoute {
     }
   }
 
-  /**
-   * @returns [Returns the Route object for HapiRouter to setup]
-   * @memberOf HelloWorldRoute
-   */
-  private createInvestment(): Hapi.RouteConfiguration {
-    return <Hapi.RouteConfiguration>{
-      path: '/investment',
-      method: 'POST',
-      config: {
-        description: 'Criar um investmento',
-        notes: 'Retorna o investimento criado',
-        tags: ['api', 'investment'],
-        handler: (req: any, reply: any) => this.create(req, reply),
-        validate: {
-          payload: {
-            refId: joi
-              .string()
-              .min(1)
-              .required(),
-          },
-        },
-      },
-    };
-  }
-
   private listInvestment(): Hapi.RouteConfiguration {
     return <Hapi.RouteConfiguration>{
       path: '/investment/{customer_id}',
@@ -130,6 +91,6 @@ export class InvestmentRoute {
   }
 
   public routes(): Hapi.RouteConfiguration[] {
-    return [this.createInvestment(), this.listInvestment()];
+    return [this.listInvestment()];
   }
 }
