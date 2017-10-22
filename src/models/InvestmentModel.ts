a4e0f71ce4560cee1109d64af69da72fbb28a019
betobarros07 <betobarros07@gmail.com>
2017-10-22 12:01:58 -0200

    Update  investment model

--- src/models/InvestmentModel.ts
+++ src/models/InvestmentModel.ts
@@ -1,21 +1,14 @@
 import mongoose from './../config/mongoose';
 
 const investmentSchema = new mongoose.Schema({
-  DT_POSICAO: String,
-  CD_CLIENTE: Number,
-  DS_CLIENTE: String,
-  CD_FUNDO: Number,
-  NM_FUNDO: String,
-  VL_PENDENTE: Number,
-  VL_APLICADO: Number,
-  VL_ATUAL: Number,
-  VL_BRUTO: Number,
-  VL_LIQUIDO: Number,
-  VL_IMPOSTO_RENDA: Number,
-  VL_RENDIMENTO: Number,
-  VL_IOF: Number,
-  VL_COTA: Number,
-  QTD_COTA: Number,
+  code: String,
+  cnpj: String,
+  name: String,
+  id: String,
+  investmentQuotation: String,
+  morningStar: String,
+  minimalInvestment: Number,
+  administrationRate: Number
 });
 
 export default mongoose.model('Investment', investmentSchema);
